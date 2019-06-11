0b9de182cbe90283425ccb88dc6e3374d23d2259
sudheer <sudheer@sudheer-PC>
2019-06-11 09:46:52 +0530

    new test case welcometest.java added

---
+++ HRMS/src/test/java/testng_scenarios/WelcomeTest.java
@@ -0,0 +1,15 @@
+package testng_scenarios;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+
+public class WelcomeTest {
+  @Test
+  public void f() {
+	  System.out.println("welcome");
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+}
